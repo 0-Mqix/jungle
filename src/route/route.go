4c88a62d9ef690c85b44b9b0f119aac7b75e946e
Mqix <maxvanmoorsel2@gmail.com>
2022-12-06 14:49:18 +0100

    new file:   go.mod 	new file:   go.sum 	new file:   src/route/route.go

---
+++ src/route/route.go
@@ -0,0 +1,12 @@
+package route
+
+import "github.com/gofiber/fiber/v2"
+
+type Handler func(*fiber.Ctx) error
+
+type Route struct {
+	Path       string
+	Method     string
+	Middleware []Handler
+	Handler    Handler
+}
