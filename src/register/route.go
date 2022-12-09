da5dd09d74af446bd25669bd14e4018066fbba29
Mqix <maxvanmoorsel2@gmail.com>
2022-12-09 09:16:22 +0100

    added a create function for routes

--- src/register/route.go
+++ src/register/route.go
@@ -8,3 +8,7 @@ type Route struct {
 	Middleware []func(*fiber.Ctx) error
 	Handler    func(*fiber.Ctx) error
 }
+
+func Create(method, path string, handler func(*fiber.Ctx) error, middleware ...func(*fiber.Ctx) error) Route {
+	return Route{Path: path, Method: method, Middleware: middleware}
+}
